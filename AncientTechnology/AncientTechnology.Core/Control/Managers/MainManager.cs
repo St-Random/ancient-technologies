31efbb913c143459962c5bd8cac1197d754603d1
yurivetroff <yuri.vetroff@gmail.com>
2016-08-27 14:10:19 +0300

    just another useless commit

--- AncientTechnology/AncientTechnology.Core/Control/Managers/MainManager.cs
+++ AncientTechnology/AncientTechnology.Core/Control/Managers/MainManager.cs
@@ -1,9 +1,6 @@
 ﻿using AncientTechnology.Core.Entities;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AncientTechnology.Core.Control.Managers
 {
