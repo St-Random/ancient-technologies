01abddda2b03c7abfa5e2754d60477215d754dcd
yurivetroff <yuri.vetroff@gmail.com>
2016-08-27 13:30:45 +0300

    added camera in Draw method

--- AncientTechnology/AncientTechnology.UI/Game.cs
+++ AncientTechnology/AncientTechnology.UI/Game.cs
@@ -1,66 +1,67 @@
-﻿using Microsoft.Xna.Framework;
+﻿using AncientTechnology.Core.Camera;
+using AncientTechnology.Core.Control.Managers;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace AncientTechnology.UI {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
-    public class Game : Microsoft.Xna.Framework.Game {
+namespace AncientTechnology.UI
+{
+    public class Game : Microsoft.Xna.Framework.Game
+    {
         GraphicsDeviceManager _graphics;
         SpriteBatch _spriteBatch;
+        MainManager _manager;
+        Camera _camera;
 
-        public Game() {
+        public Game(MainManager manager)
+        {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            _manager = manager;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
-        protected override void Initialize() {
+        protected override void Initialize()
+        {
+            var viewportWidth = GraphicsDevice.Viewport.Width;
+            var viewportHeight = GraphicsDevice.Viewport.Height;
+            _camera = new Camera(viewportWidth, viewportHeight);
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
-        protected override void LoadContent() {
+        protected override void LoadContent()
+        {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
-        protected override void UnloadContent() {
+        protected override void UnloadContent()
+        {
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Update(GameTime gameTime) {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) {
-                Exit();
-            }
-
+        protected override void Update(GameTime gameTime)
+        {
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Draw(GameTime gameTime) {
+        protected override void Draw(GameTime gameTime)
+        {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin(
+                SpriteSortMode.FrontToBack,
+                BlendState.AlphaBlend,
+                transformMatrix: _camera.Transform);
+
+            foreach (var visualObject in _manager.ObjectsToDraw)
+            {
+                _spriteBatch.Draw(
+                    visualObject.Sprite,
+                    visualObject.Position,
+                    origin: visualObject.Origin);
+            }
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
