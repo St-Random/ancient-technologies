8fafc23a6febd865b236bd837c458dc392db2a89
St-Random <st.random1024@gmail.com>
2016-08-27 13:13:11 +0300

    Moveable object injection & DI configuration

--- AncientTechnology/AncientTechnology.Core/Entities/Material/MovableObject.cs
+++ AncientTechnology/AncientTechnology.Core/Entities/Material/MovableObject.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using AncientTechnology.Core.Control.Managers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AncientTechnology.Core.Entities.Material
-{
-    public abstract class MovableObject : MaterialObject, IMovableObject
-    {
+namespace AncientTechnology.Core.Entities.Material {
+    public abstract class MovableObject : MaterialObject, IMovableObject {
+        private MainManager _manager;
+
+        public MovableObject(MainManager manager) {
+            _manager = manager;
+        }
     }
 }
--- AncientTechnology/AncientTechnology.Core/_configuration/IoCConfig.cs
+++ AncientTechnology/AncientTechnology.Core/_configuration/IoCConfig.cs
@@ -1,4 +1,5 @@
-﻿using Autofac;
+﻿using AncientTechnology.Core.Control.Managers;
+using Autofac;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,24 @@ using System.Threading.Tasks;
 namespace AncientTechnology.Core.Configuration {
     public class IoCConfig : Module {
         protected override void Load(ContainerBuilder builder) {
+            builder.RegisterAssemblyTypes()
+                   .Where(t => t.Namespace.Contains("Core.Entities")
+                               && t.IsClass && !t.IsAbstract && !t.IsInterface)
+                   .AsSelf()
+                   .InstancePerDependency();
+
+            builder.RegisterAssemblyTypes()
+                   .Where(t => t.Name.EndsWith("Controller")
+                               && t.IsClass && !t.IsAbstract && !t.IsInterface)
+                   .AsSelf()
+                   .AsImplementedInterfaces()
+                   .InstancePerLifetimeScope();
+
+
+            builder.RegisterType<MainManager>()
+                   .AsSelf()
+                   .InstancePerLifetimeScope();
+
 
         }
     }
