135eeacf4f71f1de9504101f0aa59cd36764e6be
St-Random <st.random1024@gmail.com>
2016-08-27 12:34:27 +0300

    Visual object interface

--- AncientTechnology/AncientTechnology.Core/Entities/IVisualObject.cs
+++ AncientTechnology/AncientTechnology.Core/Entities/IVisualObject.cs
@@ -1,6 +1,27 @@
-﻿namespace AncientTechnology.Core.Entities
-{
-    public interface IVisualObject : IUpdateable
-    {
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AncientTechnology.Core.Entities {
+    public interface IVisualObject : IUpdateable {
+        /// <summary>
+        /// Position of the center of the object
+        /// </summary>
+        Vector2 Position { get; set; }
+        /// <summary>
+        /// Center of the object (calulated)
+        /// </summary>
+        Vector2 Origin { get; }
+        /// <summary>
+        /// Bounds of current object (calculated)
+        /// </summary>
+        Rectangle Bounds { get; }
+        /// <summary>
+        /// A current spirte of the object
+        /// </summary>
+        Texture2D Sprite { get; }
+        /// <summary>
+        /// Object rotation in radians
+        /// </summary>
+        float Rotation { get; set; }
     }
 }
