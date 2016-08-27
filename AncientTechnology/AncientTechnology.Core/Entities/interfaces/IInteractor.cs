1a909d722d4b91a6751764c072ce81918779356d
st-random <st.random1024@gmail.com>
2016-08-27 23:30:57 +0300

    removed IThing moved interfaces to separate folder created Interactable interfaces

---
+++ AncientTechnology/AncientTechnology.Core/Entities/interfaces/IInteractor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientTechnology.Core.Entities {
+    public interface IInteractor {
+        void Interact();
+    }
+}
